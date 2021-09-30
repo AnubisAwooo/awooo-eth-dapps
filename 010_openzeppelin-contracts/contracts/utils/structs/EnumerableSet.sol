fdcb8e54326666f24936b62d69af238bd636b080
Anubis <anubisawooo@163.com>
2021-09-30 20:28:38 +0800

    EnumerableSet

--- 010_openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol
+++ 010_openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol
@@ -6,11 +6,13 @@ pragma solidity ^0.8.0;
  * @dev Library for managing
  * https://en.wikipedia.org/wiki/Set_(abstract_data_type)[sets] of primitive
  * types.
- *
+ * 集合有如下属性
  * Sets have the following properties:
  *
+ * - 元素的 增加 移除 检查是否存在 时间复杂度为 O(1)
  * - Elements are added, removed, and checked for existence in constant time
  * (O(1)).
+ * - 枚举元素
  * - Elements are enumerated in O(n). No guarantees are made on the ordering.
  *
  * ```
@@ -27,24 +29,28 @@ pragma solidity ^0.8.0;
  * and `uint256` (`UintSet`) are supported.
  */
 library EnumerableSet {
+    // 为了能够用少量代码操作尽可能多的数据类型，我们才采用 bytes32 类型作为泛型元素。
     // To implement this library for multiple types with as little code
     // repetition as possible, we write it in terms of a generic Set type with
     // bytes32 values.
+    // 集合提供私有函数 面向用户的实现
     // The Set implementation uses private functions, and user-facing
     // implementations (such as AddressSet) are just wrappers around the
     // underlying Set.
+    // 适应 bytes32 的类型才能够使用本集合
     // This means that we can only create new EnumerableSets for types that fit
     // in bytes32.
 
     struct Set {
         // Storage of set values
-        bytes32[] _values;
+        bytes32[] _values; // 所有的值
         // Position of the value in the `values` array, plus 1 because index 0
         // means a value is not in the set.
-        mapping(bytes32 => uint256) _indexes;
+        mapping(bytes32 => uint256) _indexes; // 值对应着序号，1 表示有 0 表示无
     }
 
     /**
+     * 增加元素 复杂度O(1) 私有函数
      * @dev Add a value to a set. O(1).
      *
      * Returns true if the value was added to the set, that is if it was not
@@ -52,10 +58,10 @@ library EnumerableSet {
      */
     function _add(Set storage set, bytes32 value) private returns (bool) {
         if (!_contains(set, value)) {
-            set._values.push(value);
+            set._values.push(value); // 推入这个值
             // The value is stored at length-1, but we add 1 to all indexes
             // and use 0 as a sentinel value
-            set._indexes[value] = set._values.length;
+            set._indexes[value] = set._values.length; // 映射总长度，tips 取的时候应该 - 1
             return true;
         } else {
             return false;
@@ -63,6 +69,7 @@ library EnumerableSet {
     }
 
     /**
+     * 移除元素 复杂度O(1) 私有函数
      * @dev Removes a value from a set. O(1).
      *
      * Returns true if the value was removed from the set, that is if it was
@@ -73,28 +80,31 @@ library EnumerableSet {
         uint256 valueIndex = set._indexes[value];
 
         if (valueIndex != 0) {
+            // 包含这个值
             // Equivalent to contains(set, value)
+            // 为了复杂度为O(1)的方式删除一个元素，我们采用和最后一个元素进行交换的方法，交换后移除最后一个元素就行了
             // To delete an element from the _values array in O(1), we swap the element to delete with the last one in
             // the array, and then remove the last element (sometimes called as 'swap and pop').
             // This modifies the order of the array, as noted in {at}.
 
-            uint256 toDeleteIndex = valueIndex - 1;
-            uint256 lastIndex = set._values.length - 1;
+            uint256 toDeleteIndex = valueIndex - 1; // 需要删除的序号位置
+            uint256 lastIndex = set._values.length - 1; // 上一个元素的序号
 
             if (lastIndex != toDeleteIndex) {
-                bytes32 lastvalue = set._values[lastIndex];
+                // 要删除的元素不是最后一个元素
+                bytes32 lastvalue = set._values[lastIndex]; // 取出最后一个值
 
                 // Move the last value to the index where the value to delete is
-                set._values[toDeleteIndex] = lastvalue;
+                set._values[toDeleteIndex] = lastvalue; // 将最后一个值保存到要删除位置的序号
                 // Update the index for the moved value
-                set._indexes[lastvalue] = valueIndex; // Replace lastvalue's index to valueIndex
+                set._indexes[lastvalue] = valueIndex; // Replace lastvalue's index to valueIndex 修改映射的序号关系
             }
 
             // Delete the slot where the moved value was stored
-            set._values.pop();
+            set._values.pop(); // 弹出最后一个值
 
             // Delete the index for the deleted slot
-            delete set._indexes[value];
+            delete set._indexes[value]; // 删除最后一个值的映射
 
             return true;
         } else {
@@ -103,13 +113,15 @@ library EnumerableSet {
     }
 
     /**
+     * 是否包含某元素 复杂度O(1) 私有函数 只读
      * @dev Returns true if the value is in the set. O(1).
      */
     function _contains(Set storage set, bytes32 value) private view returns (bool) {
-        return set._indexes[value] != 0;
+        return set._indexes[value] != 0; // 对应的映射序号不为 0 表示有这个值
     }
 
     /**
+     * 获取集合元素个数 私有函数 只读
      * @dev Returns the number of values on the set. O(1).
      */
     function _length(Set storage set) private view returns (uint256) {
@@ -117,8 +129,10 @@ library EnumerableSet {
     }
 
     /**
+     * 通过序号获取元素 私有函数 只读
      * @dev Returns the value stored at position `index` in the set. O(1).
      *
+     * 注意 这里不保证顺序 在增加和移除的过程中，元素存储位置的顺序可能会被改变
      * Note that there are no guarantees on the ordering of values inside the
      * array, and it may change when more values are added or removed.
      *
@@ -131,8 +145,11 @@ library EnumerableSet {
     }
 
     /**
+     * 获取所有的元素数组 私有函数 只读
      * @dev Return the entire set in an array
      *
+     * 警告 这个操作会从存储复制到内存，相当耗费 gas。这个函数设计出来大概率是给只读函数调用的，这时不需要 gas 费。
+     * 开发者应该时刻记住这个方法有很大的成本，使用本函数作为状态改变调用可能会导致函数无法调用，因为复制到内存可能直接把整个块的 gas 限制用完了
      * WARNING: This operation will copy the entire storage to memory, which can be quite expensive. This is designed
      * to mostly be used by view accessors that are queried without any gas fees. Developers should keep in mind that
      * this function has an unbounded cost, and using it as part of a state-changing function may render the function
@@ -149,6 +166,7 @@ library EnumerableSet {
     }
 
     /**
+     * 增加元素 内部函数
      * @dev Add a value to a set. O(1).
      *
      * Returns true if the value was added to the set, that is if it was not
@@ -159,6 +177,7 @@ library EnumerableSet {
     }
 
     /**
+     * 移除元素 内部函数
      * @dev Removes a value from a set. O(1).
      *
      * Returns true if the value was removed from the set, that is if it was
@@ -169,6 +188,7 @@ library EnumerableSet {
     }
 
     /**
+     * 是否包含 内部函数 只读
      * @dev Returns true if the value is in the set. O(1).
      */
     function contains(Bytes32Set storage set, bytes32 value) internal view returns (bool) {
@@ -176,6 +196,7 @@ library EnumerableSet {
     }
 
     /**
+     * 集合大小 内部函数 只读
      * @dev Returns the number of values in the set. O(1).
      */
     function length(Bytes32Set storage set) internal view returns (uint256) {
@@ -183,6 +204,7 @@ library EnumerableSet {
     }
 
     /**
+     * 按序号读取值 内部函数 只读
      * @dev Returns the value stored at position `index` in the set. O(1).
      *
      * Note that there are no guarantees on the ordering of values inside the
@@ -197,6 +219,7 @@ library EnumerableSet {
     }
 
     /**
+     * 所有值数组 内部函数 只读
      * @dev Return the entire set in an array
      *
      * WARNING: This operation will copy the entire storage to memory, which can be quite expensive. This is designed
@@ -215,6 +238,7 @@ library EnumerableSet {
     }
 
     /**
+     * 增加地址 内部函数
      * @dev Add a value to a set. O(1).
      *
      * Returns true if the value was added to the set, that is if it was not
@@ -225,6 +249,7 @@ library EnumerableSet {
     }
 
     /**
+     * 移除地址 内部函数
      * @dev Removes a value from a set. O(1).
      *
      * Returns true if the value was removed from the set, that is if it was
@@ -235,6 +260,7 @@ library EnumerableSet {
     }
 
     /**
+     * 是否包含地址 内部函数 只读
      * @dev Returns true if the value is in the set. O(1).
      */
     function contains(AddressSet storage set, address value) internal view returns (bool) {
@@ -242,6 +268,7 @@ library EnumerableSet {
     }
 
     /**
+     * 地址个数 内部函数 只读
      * @dev Returns the number of values in the set. O(1).
      */
     function length(AddressSet storage set) internal view returns (uint256) {
@@ -249,6 +276,7 @@ library EnumerableSet {
     }
 
     /**
+     * 按序号读取值 内部函数 只读
      * @dev Returns the value stored at position `index` in the set. O(1).
      *
      * Note that there are no guarantees on the ordering of values inside the
@@ -263,6 +291,7 @@ library EnumerableSet {
     }
 
     /**
+     * 所有值 内部函数 只读
      * @dev Return the entire set in an array
      *
      * WARNING: This operation will copy the entire storage to memory, which can be quite expensive. This is designed
@@ -288,6 +317,7 @@ library EnumerableSet {
     }
 
     /**
+     * 增加数字 内部函数
      * @dev Add a value to a set. O(1).
      *
      * Returns true if the value was added to the set, that is if it was not
@@ -298,6 +328,7 @@ library EnumerableSet {
     }
 
     /**
+     * 移除数字 内部函数
      * @dev Removes a value from a set. O(1).
      *
      * Returns true if the value was removed from the set, that is if it was
@@ -308,6 +339,7 @@ library EnumerableSet {
     }
 
     /**
+     * 是否包含 内部函数 只读
      * @dev Returns true if the value is in the set. O(1).
      */
     function contains(UintSet storage set, uint256 value) internal view returns (bool) {
@@ -315,6 +347,7 @@ library EnumerableSet {
     }
 
     /**
+     * 长度 内部函数 只读
      * @dev Returns the number of values on the set. O(1).
      */
     function length(UintSet storage set) internal view returns (uint256) {
@@ -322,6 +355,7 @@ library EnumerableSet {
     }
 
     /**
+     * 按序号读值 内部函数 只读
      * @dev Returns the value stored at position `index` in the set. O(1).
      *
      * Note that there are no guarantees on the ordering of values inside the
@@ -336,6 +370,7 @@ library EnumerableSet {
     }
 
     /**
+     * 所有值 内部函数 只读
      * @dev Return the entire set in an array
      *
      * WARNING: This operation will copy the entire storage to memory, which can be quite expensive. This is designed
