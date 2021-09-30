f276cfdddd9ca2b6bab431a256e62a7367083a7f
Anubis <anubisawooo@163.com>
2021-09-30 17:40:43 +0800

    ERC165

--- 010_openzeppelin-contracts/contracts/utils/introspection/ERC165.sol
+++ 010_openzeppelin-contracts/contracts/utils/introspection/ERC165.sol
@@ -5,8 +5,10 @@ pragma solidity ^0.8.0;
 import "./IERC165.sol";
 
 /**
+ * ERC165 标准的具体实现
  * @dev Implementation of the {IERC165} interface.
  *
+ * 想实现 ERC165 标准的合约应当继承本合约并且复写 supportsInterface 方法，方能为其他想要实现的接口提供 ERC165 标准的支持
  * Contracts that want to implement ERC165 should inherit from this contract and override {supportsInterface} to check
  * for the additional interface id that will be supported. For example:
  *
