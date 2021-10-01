8843b1c7c857f782ef1ce80424eadd96ab737cd7
Anubis <anubisawooo@163.com>
2021-10-01 13:00:43 +0800

    Address

--- 010_openzeppelin-contracts/contracts/utils/Address.sol
+++ 010_openzeppelin-contracts/contracts/utils/Address.sol
@@ -3,24 +3,27 @@
 pragma solidity ^0.8.0;
 
 /**
+ * 和地址相关的函数集合
  * @dev Collection of functions related to the address type
  */
 library Address {
     /**
+     * 地址是否合约地址 内部函数 只读
      * @dev Returns true if `account` is a contract.
      *
      * [IMPORTANT]
      * ====
+     * 外部账户和并非合约，都会返回 false
      * It is unsafe to assume that an address for which this function returns
      * false is an externally-owned account (EOA) and not a contract.
      *
      * Among others, `isContract` will return false for the following
      * types of addresses:
      *
-     *  - an externally-owned account
-     *  - a contract in construction
-     *  - an address where a contract will be created
-     *  - an address where a contract lived, but was destroyed
+     *  - an externally-owned account  外部账户
+     *  - a contract in construction  构建中的合约
+     *  - an address where a contract will be created 合约即将创建的账户
+     *  - an address where a contract lived, but was destroyed  被销毁的合约
      * ====
      */
     function isContract(address account) internal view returns (bool) {
@@ -32,10 +35,11 @@ library Address {
         assembly {
             size := extcodesize(account)
         }
-        return size > 0;
+        return size > 0; // 地址账户上的代码大小不为 0  就是有代码
     }
 
     /**
+     * 替换 solidity 原本的 transfer 方法，给指定合约发送有以太币  内部函数
      * @dev Replacement for Solidity's `transfer`: sends `amount` wei to
      * `recipient`, forwarding all available gas and reverting on errors.
      *
@@ -43,29 +47,34 @@ library Address {
      * of certain opcodes, possibly making contracts go over the 2300 gas limit
      * imposed by `transfer`, making them unable to receive funds via
      * `transfer`. {sendValue} removes this limitation.
+     * EIP1884更改了操作指令消耗的 gas，采用 transfer 有可能更导致合约超过 2300 的 gaslimit，导致发送失败，本方法无此限制
      *
      * https://diligence.consensys.net/posts/2019/09/stop-using-soliditys-transfer-now/[Learn more].
      *
+     * 重要：因为转账把控制权交给其他地址，必须防止重入攻击，考虑使用 ReentrancyGuard 模式或参考...
      * IMPORTANT: because control is transferred to `recipient`, care must be
      * taken to not create reentrancy vulnerabilities. Consider using
      * {ReentrancyGuard} or the
      * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
      */
     function sendValue(address payable recipient, uint256 amount) internal {
-        require(address(this).balance >= amount, "Address: insufficient balance");
+        require(address(this).balance >= amount, "Address: insufficient balance"); // 检查发送方余额够不够
 
-        (bool success, ) = recipient.call{value: amount}("");
-        require(success, "Address: unable to send value, recipient may have reverted");
+        (bool success, ) = recipient.call{value: amount}(""); // 这里直接调用 call 方法，携带数量 amount，没有写调用的方法名
+        require(success, "Address: unable to send value, recipient may have reverted"); // 发送失败提示
     }
 
     /**
+     * 底层代码 call 方法的调用演示 直接使用 call 是不安全的函数调用，使用本函数替代  内部函数
      * @dev Performs a Solidity function call using a low level `call`. A
      * plain `call` is an unsafe replacement for a function call: use this
      * function instead.
      *
+     * 如果目标合约调用出现异常，本函数会冒泡传递错误
      * If `target` reverts with a revert reason, it is bubbled up by this
      * function (like regular Solidity function calls).
      *
+     * 返回原始返回的数据，如果想转换为期望的值，使用...
      * Returns the raw returned data. To convert to the expected return value,
      * use https://solidity.readthedocs.io/en/latest/units-and-global-variables.html?highlight=abi.decode#abi-encoding-and-decoding-functions[`abi.decode`].
      *
@@ -81,6 +90,7 @@ library Address {
     }
 
     /**
+     * 指定错误名称 内部函数
      * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`], but with
      * `errorMessage` as a fallback revert reason when `target` reverts.
      *
@@ -95,6 +105,7 @@ library Address {
     }
 
     /**
+     * 携带以太币调用 内部函数
      * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
      * but also transferring `value` wei to `target`.
      *
@@ -114,6 +125,7 @@ library Address {
     }
 
     /**
+     * 全参数
      * @dev Same as {xref-Address-functionCallWithValue-address-bytes-uint256-}[`functionCallWithValue`], but
      * with `errorMessage` as a fallback revert reason when `target` reverts.
      *
@@ -125,14 +137,15 @@ library Address {
         uint256 value,
         string memory errorMessage
     ) internal returns (bytes memory) {
-        require(address(this).balance >= value, "Address: insufficient balance for call");
-        require(isContract(target), "Address: call to non-contract");
+        require(address(this).balance >= value, "Address: insufficient balance for call"); // 检查发送者余额
+        require(isContract(target), "Address: call to non-contract"); // 目标地址必须是合约
 
-        (bool success, bytes memory returndata) = target.call{value: value}(data);
+        (bool success, bytes memory returndata) = target.call{value: value}(data); // 调用函数 方法签名和参数都在 data 里
         return verifyCallResult(success, returndata, errorMessage);
     }
 
     /**
+     * 静态函数调用 内部函数 只读
      * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
      * but performing a static call.
      *
@@ -143,6 +156,7 @@ library Address {
     }
 
     /**
+     * 静态函数调用 内部函数 只读
      * @dev Same as {xref-Address-functionCall-address-bytes-string-}[`functionCall`],
      * but performing a static call.
      *
@@ -155,11 +169,12 @@ library Address {
     ) internal view returns (bytes memory) {
         require(isContract(target), "Address: static call to non-contract");
 
-        (bool success, bytes memory returndata) = target.staticcall(data);
+        (bool success, bytes memory returndata) = target.staticcall(data); // 静态调用
         return verifyCallResult(success, returndata, errorMessage);
     }
 
     /**
+     * 委托调用 内部函数 和 call 区别是 使用别人的代码在自己的内存上运行
      * @dev Same as {xref-Address-functionCall-address-bytes-}[`functionCall`],
      * but performing a delegate call.
      *
@@ -170,6 +185,7 @@ library Address {
     }
 
     /**
+     * 委托调用 内部函数
      * @dev Same as {xref-Address-functionCall-address-bytes-string-}[`functionCall`],
      * but performing a delegate call.
      *
@@ -187,6 +203,7 @@ library Address {
     }
 
     /**
+     * 验证调用结果 内部函数 纯函数
      * @dev Tool to verifies that a low level call was successful, and revert if it wasn't, either by bubbling the
      * revert reason using the provided one.
      *
