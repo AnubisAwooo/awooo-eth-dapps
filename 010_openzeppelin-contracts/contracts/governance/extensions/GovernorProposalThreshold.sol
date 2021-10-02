a523f115a282c6b9d3ec464e4d474ea5be75eb4c
Anubis <anubisawooo@163.com>
2021-10-02 11:24:20 +0800

    GovernorProposalThreshold

--- 010_openzeppelin-contracts/contracts/governance/extensions/GovernorProposalThreshold.sol
+++ 010_openzeppelin-contracts/contracts/governance/extensions/GovernorProposalThreshold.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "../Governor.sol";
 
 /**
+ * 拓展合约 Governor 支持提案限制持有者最小余额
  * @dev Extension of {Governor} for proposal restriction to token holders with a minimum balance.
  *
  * _Available since v4.3._
@@ -28,6 +29,7 @@ abstract contract GovernorProposalThreshold is Governor {
     }
 
     /**
+     * 有一定支持人数的人才能发起提案
      * @dev Part of the Governor Bravo's interface: _"The number of votes required in order for a voter to become a proposer"_.
      */
     function proposalThreshold() public view virtual returns (uint256);
