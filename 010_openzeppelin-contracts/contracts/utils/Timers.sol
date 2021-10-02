5f85aab50b9b99caa98d63762613bfe104fd9431
Anubis <anubisawooo@163.com>
2021-10-02 09:27:54 +0800

    Timers

--- 010_openzeppelin-contracts/contracts/utils/Timers.sol
+++ 010_openzeppelin-contracts/contracts/utils/Timers.sol
@@ -3,41 +3,51 @@
 pragma solidity ^0.8.0;
 
 /**
+ * 时间戳 定时器 延时 工具
  * @dev Tooling for timepoints, timers and delays
  */
 library Timers {
+    // 时间戳结构体
     struct Timestamp {
         uint64 _deadline;
     }
 
+    // 获取截止时间
     function getDeadline(Timestamp memory timer) internal pure returns (uint64) {
         return timer._deadline;
     }
 
+    // 设置截止时间
     function setDeadline(Timestamp storage timer, uint64 timestamp) internal {
         timer._deadline = timestamp;
     }
 
+    // 重置截止时间
     function reset(Timestamp storage timer) internal {
         timer._deadline = 0;
     }
 
+    // 是否未被重置
     function isUnset(Timestamp memory timer) internal pure returns (bool) {
         return timer._deadline == 0;
     }
 
+    // 是否开始计时
     function isStarted(Timestamp memory timer) internal pure returns (bool) {
         return timer._deadline > 0;
     }
 
+    // 不明白这是啥意思
     function isPending(Timestamp memory timer) internal view returns (bool) {
         return timer._deadline > block.timestamp;
     }
 
+    // 是否过期
     function isExpired(Timestamp memory timer) internal view returns (bool) {
         return isStarted(timer) && timer._deadline <= block.timestamp;
     }
 
+    // 块序号结构体 感觉像是不估计块的数量，通过时间戳比较
     struct BlockNumber {
         uint64 _deadline;
     }
