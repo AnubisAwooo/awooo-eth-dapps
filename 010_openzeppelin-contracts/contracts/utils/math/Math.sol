751abef9cd178ec31b086a0f9ccdcb62fa666142
Anubis <anubisawooo@163.com>
2021-10-03 01:50:20 +0800

    Math

--- 010_openzeppelin-contracts/contracts/utils/math/Math.sol
+++ 010_openzeppelin-contracts/contracts/utils/math/Math.sol
@@ -3,10 +3,12 @@
 pragma solidity ^0.8.0;
 
 /**
+ * 数学标准库
  * @dev Standard math utilities missing in the Solidity language.
  */
 library Math {
     /**
+     * 最大值
      * @dev Returns the largest of two numbers.
      */
     function max(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -14,6 +16,7 @@ library Math {
     }
 
     /**
+     * 最小值
      * @dev Returns the smallest of two numbers.
      */
     function min(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -21,6 +24,7 @@ library Math {
     }
 
     /**
+     * 平均数 这个算法有点意思 & 取的都是 1 的位 ^ 异或运算把其中一个位是 1 的取出再右移 1 位
      * @dev Returns the average of two numbers. The result is rounded towards
      * zero.
      */
@@ -30,6 +34,7 @@ library Math {
     }
 
     /**
+     * 天花板除法，有余数都进一  tips 有可能 b 是 0 呢
      * @dev Returns the ceiling of the division of two numbers.
      *
      * This differs from standard division with `/` in that it rounds up instead
