53644735d83c2f8cdc0c58fd581e090d48e90c7a
Anubis <anubisawooo@163.com>
2021-10-03 00:24:52 +0800

    IERC20Permit

--- 010_openzeppelin-contracts/contracts/token/ERC20/extensions/draft-IERC20Permit.sol
+++ 010_openzeppelin-contracts/contracts/token/ERC20/extensions/draft-IERC20Permit.sol
@@ -3,15 +3,19 @@
 pragma solidity ^0.8.0;
 
 /**
+ * 拓展 ERC20 标准 允许通过签名授权 见 EIP2612
  * @dev Interface of the ERC20 Permit extension allowing approvals to be made via signatures, as defined in
  * https://eips.ethereum.org/EIPS/eip-2612[EIP-2612].
  *
+ * 增加许可方法，可以用来改变账户 ERC20 的 allowance 方法通过账户签名的消息。由于不是 ERC20 的方法，token 持有者不必发送交易，仅此也不必要求账户持有 eth。
+ * tips: 这样一个地址可以直接只接收代币，签名授权额度即可，完全不必连线以太坊操作。
  * Adds the {permit} method, which can be used to change an account's ERC20 allowance (see {IERC20-allowance}) by
  * presenting a message signed by the account. By not relying on {IERC20-approve}, the token holder account doesn't
  * need to send a transaction, and thus is not required to hold Ether at all.
  */
 interface IERC20Permit {
     /**
+     * 许可方法
      * @dev Sets `value` as the allowance of `spender` over ``owner``'s tokens,
      * given ``owner``'s signed approval.
      *
@@ -43,15 +47,18 @@ interface IERC20Permit {
     ) external;
 
     /**
+     * 获取地址的 nonce 这个值必须在签名里使用
      * @dev Returns the current nonce for `owner`. This value must be
      * included whenever a signature is generated for {permit}.
      *
+     * 每次成功的调用 permit 方法都会让这个 owner 的 nonce 加 1，这样防止同样的签名被用多次。
      * Every successful call to {permit} increases ``owner``'s nonce by one. This
      * prevents a signature from being used multiple times.
      */
     function nonces(address owner) external view returns (uint256);
 
     /**
+     * 返回域分隔符
      * @dev Returns the domain separator used in the encoding of the signature for {permit}, as defined by {EIP712}.
      */
     // solhint-disable-next-line func-name-mixedcase
