f190913162ffb2d7dbf01739d3cdb992545e8a36
Anubis <anubisawooo@163.com>
2021-10-03 21:58:44 +0800

    ERC721Enumerable

--- 010_openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol
+++ 010_openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol
@@ -6,6 +6,7 @@ import "../ERC721.sol";
 import "./IERC721Enumerable.sol";
 
 /**
+ * ERC721 带枚举
  * @dev This implements an optional extension of {ERC721} defined in the EIP that adds
  * enumerability of all the token ids in the contract as well as all token ids owned by each
  * account.
