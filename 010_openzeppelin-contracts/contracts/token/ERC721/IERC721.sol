29ba4058cb75596f6ef544d91dcfac5cd7d8dc15
Anubis <anubisawooo@163.com>
2021-10-03 22:16:13 +0800

    IERC721

--- 010_openzeppelin-contracts/contracts/token/ERC721/IERC721.sol
+++ 010_openzeppelin-contracts/contracts/token/ERC721/IERC721.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "../../utils/introspection/IERC165.sol";
 
 /**
+ * ERC721 接口
  * @dev Required interface of an ERC721 compliant contract.
  */
 interface IERC721 is IERC165 {
