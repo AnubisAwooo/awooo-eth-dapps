a822eb3f3829d5e3bc6b26ac475b7889cb89b5fa
Anubis <anubisawooo@163.com>
2021-10-04 02:37:24 +0800

    Create2

--- 010_openzeppelin-contracts/contracts/utils/Create2.sol
+++ 010_openzeppelin-contracts/contracts/utils/Create2.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 /**
+ * CREATE2 指令工具
  * @dev Helper to make usage of the `CREATE2` EVM opcode easier and safer.
  * `CREATE2` can be used to compute in advance the address where a smart
  * contract will be deployed, which allows for interesting new mechanisms known
