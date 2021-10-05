686802579157b9eeaa7c3b6b1f3283b285d09f9d
Anubis <anubisawooo@163.com>
2021-10-06 03:41:24 +0800

    InterestRateModel

--- 015_CErc20Delegator/contracts/InterestRateModel.sol
+++ 015_CErc20Delegator/contracts/InterestRateModel.sol
@@ -1,14 +1,17 @@
 pragma solidity ^0.5.16;
 
 /**
+ * Compound 的利率模型接口
  * @title Compound's InterestRateModel Interface
  * @author Compound
  */
 contract InterestRateModel {
+    // 标记是利率模型
     /// @notice Indicator that this is an InterestRateModel contract (for inspection)
     bool public constant isInterestRateModel = true;
 
     /**
+     * 计算当前借款利率 每个块？
      * @notice Calculates the current borrow interest rate per block
      * @param cash The total amount of cash the market has
      * @param borrows The total amount of borrows the market has outstanding
@@ -22,6 +25,7 @@ contract InterestRateModel {
     ) external view returns (uint256);
 
     /**
+     * 计算当前存款利率 每个块？
      * @notice Calculates the current supply interest rate per block
      * @param cash The total amount of cash the market has
      * @param borrows The total amount of borrows the market has outstanding
