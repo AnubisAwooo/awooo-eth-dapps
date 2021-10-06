01d3ed8d884f80d0522280f12bd4265d362681a5
Anubis <anubisawooo@163.com>
2021-10-06 19:00:13 +0800

    Cerc20Delegator

--- 015_CErc20Delegator/contracts/Cerc20Delegator.sol
+++ 015_CErc20Delegator/contracts/Cerc20Delegator.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.16;
 import "./CTokenInterfaces.sol";
 
 /**
+ * Compound 的 C 代币委托合约
  * @title Compound's CErc20Delegator Contract
  * @notice CTokens which wrap an EIP-20 underlying and delegate to an implementation
  * @author Compound
@@ -14,16 +15,16 @@ contract CErc20Delegator is
 {
     /**
      * @notice Construct a new money market
-     * @param underlying_ The address of the underlying asset
-     * @param comptroller_ The address of the Comptroller
-     * @param interestRateModel_ The address of the interest rate model
-     * @param initialExchangeRateMantissa_ The initial exchange rate, scaled by 1e18
-     * @param name_ ERC-20 name of this token
-     * @param symbol_ ERC-20 symbol of this token
-     * @param decimals_ ERC-20 decimal precision of this token
-     * @param admin_ Address of the administrator of this token
-     * @param implementation_ The address of the implementation the contract delegates to
-     * @param becomeImplementationData The encoded args for becomeImplementation
+     * @param underlying_ The address of the underlying asset 被代理的真正的代币合约地址
+     * @param comptroller_ The address of the Comptroller 审计合约地址
+     * @param interestRateModel_ The address of the interest rate model 利率模型合约地址
+     * @param initialExchangeRateMantissa_ The initial exchange rate, scaled by 1e18 初始汇率？ 1 代币兑换 10^18 本代币
+     * @param name_ ERC-20 name of this token 本代币名称
+     * @param symbol_ ERC-20 symbol of this token 本代币符号
+     * @param decimals_ ERC-20 decimal precision of this token 本代币精度
+     * @param admin_ Address of the administrator of this token 管理员地址
+     * @param implementation_ The address of the implementation the contract delegates to 实现合约功能的被代理的合约地址
+     * @param becomeImplementationData The encoded args for becomeImplementation 编码后的成为被代理的合约需要的数据？？
      */
     constructor(
         address underlying_,
@@ -37,9 +38,11 @@ contract CErc20Delegator is
         address implementation_,
         bytes memory becomeImplementationData
     ) public {
+        // 初始化时部署者是管理员
         // Creator of the contract is admin during initialization
         admin = msg.sender;
 
+        // 第一次委托，要进行初始化
         // First delegate gets to initialize the delegator (i.e. storage contract)
         delegateTo(
             implementation_,
@@ -55,17 +58,20 @@ contract CErc20Delegator is
             )
         );
 
+        // 总是通过设置函数的方式设置代理
         // New implementations always get set via the settor (post-initialize)
         _setImplementation(implementation_, false, becomeImplementationData);
 
+        // 初始化完成后 设置指定地址为管理员
         // Set the proper admin now that initialization is done
         admin = admin_;
     }
 
     /**
+     * 设置被代理的合约
      * @notice Called by the admin to update the implementation of the delegator
      * @param implementation_ The address of the new implementation for delegation
-     * @param allowResign Flag to indicate whether to call _resignImplementation on the old implementation
+     * @param allowResign Flag to indicate whether to call _resignImplementation on the old implementation 是否调用旧代理的会回调函数
      * @param becomeImplementationData The encoded bytes data to be passed to _becomeImplementation
      */
     function _setImplementation(
@@ -74,11 +80,12 @@ contract CErc20Delegator is
         bytes memory becomeImplementationData
     ) public {
         require(
-            msg.sender == admin,
+            msg.sender == admin, // 要求调用者必须是管理员
             "CErc20Delegator::_setImplementation: Caller must be admin"
         );
 
         if (allowResign) {
+            // 如果需要调用上个被代理合约的回调函数
             delegateToImplementation(
                 abi.encodeWithSignature("_resignImplementation()")
             );
@@ -87,6 +94,7 @@ contract CErc20Delegator is
         address oldImplementation = implementation;
         implementation = implementation_;
 
+        // 调用新代理的成为实现函数
         delegateToImplementation(
             abi.encodeWithSignature(
                 "_becomeImplementation(bytes)",
@@ -94,24 +102,26 @@ contract CErc20Delegator is
             )
         );
 
-        emit NewImplementation(oldImplementation, implementation);
+        emit NewImplementation(oldImplementation, implementation); // 触发具体实现更改事件
     }
 
     /**
+     * 调用者提供资产进入市场并且作为交换收到代理代币
      * @notice Sender supplies assets into the market and receives cTokens in exchange
      * @dev Accrues interest whether or not the operation succeeds, unless reverted
-     * @param mintAmount The amount of the underlying asset to supply
-     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+     * @param mintAmount The amount of the underlying asset to supply // 抵押的数量
+     * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details) // 返回值 0 就是成功
      */
     function mint(uint256 mintAmount) external returns (uint256) {
         mintAmount; // Shh
-        delegateAndReturn();
+        delegateAndReturn(); // 这个方法做了什么？
     }
 
     /**
+     * 发送者还回代理代币并赎回抵押代币
      * @notice Sender redeems cTokens in exchange for the underlying asset
      * @dev Accrues interest whether or not the operation succeeds, unless reverted
-     * @param redeemTokens The number of cTokens to redeem into underlying
+     * @param redeemTokens The number of cTokens to redeem into underlying // 还回的代理代币数量
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
      */
     function redeem(uint256 redeemTokens) external returns (uint256) {
@@ -120,6 +130,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 赎回指定数量的抵押代币
      * @notice Sender redeems cTokens in exchange for a specified amount of underlying asset
      * @dev Accrues interest whether or not the operation succeeds, unless reverted
      * @param redeemAmount The amount of underlying to redeem
@@ -131,8 +142,9 @@ contract CErc20Delegator is
     }
 
     /**
+     * 调用者接入资产到自己的地址
      * @notice Sender borrows assets from the protocol to their own address
-     * @param borrowAmount The amount of the underlying asset to borrow
+     * @param borrowAmount The amount of the underlying asset to borrow // 想要借入资产的数量
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
      */
     function borrow(uint256 borrowAmount) external returns (uint256) {
@@ -141,6 +153,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 调用者偿还借入的资产
      * @notice Sender repays their own borrow
      * @param repayAmount The amount to repay
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
@@ -151,6 +164,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 调用者偿还属于借款人的借款  这里的意思应该是另外的地址替别人还款并赎回质押
      * @notice Sender repays a borrow belonging to borrower
      * @param borrower the account with the debt being payed off
      * @param repayAmount The amount to repay
@@ -160,17 +174,18 @@ contract CErc20Delegator is
         external
         returns (uint256)
     {
-        borrower;
+        borrower; // 感觉这种语法像是压入栈
         repayAmount; // Shh
         delegateAndReturn();
     }
 
     /**
+     * 调用者清算借款人担保物
      * @notice The sender liquidates the borrowers collateral.
-     *  The collateral seized is transferred to the liquidator.
-     * @param borrower The borrower of this cToken to be liquidated
-     * @param cTokenCollateral The market in which to seize collateral from the borrower
-     * @param repayAmount The amount of the underlying borrowed asset to repay
+     *  The collateral seized is transferred to the liquidator. 担保物扣押转移给清算人
+     * @param borrower The borrower of this cToken to be liquidated 将要被清算的借款人地址
+     * @param cTokenCollateral The market in which to seize collateral from the borrower 从借款人那里夺取抵押品的市场
+     * @param repayAmount The amount of the underlying borrowed asset to repay 帮借款人偿还的资产
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
      */
     function liquidateBorrow(
@@ -185,6 +200,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 转账
      * @notice Transfer `amount` tokens from `msg.sender` to `dst`
      * @param dst The address of the destination account
      * @param amount The number of tokens to transfer
@@ -197,6 +213,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 通过授权转账
      * @notice Transfer `amount` tokens from `src` to `dst`
      * @param src The address of the source account
      * @param dst The address of the destination account
@@ -215,6 +232,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 授权
      * @notice Approve `spender` to transfer up to `amount` from `src`
      * @dev This will overwrite the approval amount for `spender`
      *  and is subject to issues noted [here](https://eips.ethereum.org/EIPS/eip-20#approve)
@@ -229,6 +247,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 查询授权
      * @notice Get the current allowance from `owner` for `spender`
      * @param owner The address of the account which owns the tokens to be spent
      * @param spender The address of the account which may transfer tokens
@@ -245,6 +264,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 查询余额
      * @notice Get the token balance of the `owner`
      * @param owner The address of the account to query
      * @return The number of tokens owned by `owner`
@@ -255,6 +275,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 查询抵押代币余额
      * @notice Get the underlying balance of the `owner`
      * @dev This also accrues interest in a transaction
      * @param owner The address of the account to query
@@ -266,6 +287,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 获取账户余额的快照？
      * @notice Get a snapshot of the account's balances, and the cached exchange rate
      * @dev This is used by comptroller to more efficiently perform liquidity checks.
      * @param account Address of the account to snapshot
@@ -286,6 +308,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 获取当前借款利率 单位是 每块
      * @notice Returns the current per-block borrow interest rate for this cToken
      * @return The borrow interest rate per block, scaled by 1e18
      */
@@ -294,6 +317,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 获取当前存储利率 单位是 每块
      * @notice Returns the current per-block supply interest rate for this cToken
      * @return The supply interest rate per block, scaled by 1e18
      */
@@ -302,6 +326,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 获取当前总借款
      * @notice Returns the current total borrows plus accrued interest
      * @return The total borrows with interest
      */
@@ -310,6 +335,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 获取当前某账户的总借款
      * @notice Accrue interest to updated borrowIndex and then calculate account's borrow balance using the updated borrowIndex
      * @param account The address whose balance should be calculated after updating borrowIndex
      * @return The calculated balance
@@ -320,6 +346,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 不知道和上一个有什么区别？？查询的位置不一样？
      * @notice Return the borrow balance of account based on stored data
      * @param account The address whose balance should be calculated
      * @return The calculated balance
@@ -334,6 +361,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 当前汇率 ？
      * @notice Accrue interest then return the up-to-date exchange rate
      * @return Calculated exchange rate scaled by 1e18
      */
@@ -351,6 +379,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 获取标的资产中此 cToken 的现金余额 谷歌翻译 不懂啥意思 属于本合约的代币资产？
      * @notice Get cash balance of this cToken in the underlying asset
      * @return The quantity of underlying asset owned by this contract
      */
@@ -359,6 +388,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 将应计利息应用于总借款和准备金。
      * @notice Applies accrued interest to total borrows and reserves.
      * @dev This calculates interest accrued from the last checkpointed block
      *      up to the current block and writes new checkpoint to storage.
@@ -368,6 +398,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 扣押质押资产
      * @notice Transfers collateral tokens (this market) to the liquidator.
      * @dev Will fail unless called by another cToken during the process of liquidation.
      *  Its absolutely critical to use msg.sender as the borrowed cToken and not a parameter.
@@ -388,8 +419,10 @@ contract CErc20Delegator is
     }
 
     /*** Admin Functions ***/
+    // 管理员调用的方法
 
     /**
+     * 设置新的候选管理员  候选地址必须调用接受管理来结束转账？？
      * @notice Begins transfer of admin rights. The newPendingAdmin must call `_acceptAdmin` to finalize the transfer.
      * @dev Admin function to begin change of admin. The newPendingAdmin must call `_acceptAdmin` to finalize the transfer.
      * @param newPendingAdmin New pending admin.
@@ -404,6 +437,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 设置审计合约
      * @notice Sets a new comptroller for the market
      * @dev Admin function to set a new comptroller
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
@@ -417,6 +451,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 设置质押率
      * @notice accrues interest and sets a new reserve factor for the protocol using _setReserveFactorFresh
      * @dev Admin function to accrue interest and set a new reserve factor
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
@@ -430,6 +465,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 接受管理员权限
      * @notice Accepts transfer of admin rights. msg.sender must be pendingAdmin
      * @dev Admin function for pending admin to accept role and update admin
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
@@ -439,6 +475,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 增加准备金？？
      * @notice Accrues interest and adds reserves by transferring from admin
      * @param addAmount Amount of reserves to add
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
@@ -449,6 +486,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 减少准备金？？
      * @notice Accrues interest and reduces reserves by transferring to admin
      * @param reduceAmount Amount of reduction to reserves
      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
@@ -459,6 +497,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 这是利率模型合约
      * @notice Accrues interest and updates the interest rate model using _setInterestRateModelFresh
      * @dev Admin function to accrue interest and update the interest rate model
      * @param newInterestRateModel the new interest rate model to use
@@ -473,6 +512,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 代理给具体执行合约
      * @notice Internal method to delegate execution to another contract
      * @dev It returns to the external caller whatever the implementation returns or forwards reverts
      * @param callee The contract to delegatecall
@@ -483,7 +523,7 @@ contract CErc20Delegator is
         internal
         returns (bytes memory)
     {
-        (bool success, bytes memory returnData) = callee.delegatecall(data);
+        (bool success, bytes memory returnData) = callee.delegatecall(data); // 执行合约调用方法 并返回结果
         assembly {
             if eq(success, 0) {
                 revert(add(returnData, 0x20), returndatasize)
@@ -493,6 +533,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 委托执行
      * @notice Delegates execution to the implementation contract
      * @dev It returns to the external caller whatever the implementation returns or forwards reverts
      * @param data The raw data to delegatecall
@@ -506,6 +547,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 委托执行 只读执行 实际上保证一层还是调用了 delegateToImplementation
      * @notice Delegates execution to an implementation contract
      * @dev It returns to the external caller whatever the implementation returns or forwards reverts
      *  There are an additional 2 prefix uints from the wrapper returndata, which we ignore since we make an extra hop.
@@ -528,6 +570,7 @@ contract CErc20Delegator is
         return abi.decode(returnData, (bytes));
     }
 
+    // 委托只读执行
     function delegateToViewAndReturn() private view returns (bytes memory) {
         (bool success, ) = address(this).staticcall(
             abi.encodeWithSignature("delegateToImplementation(bytes)", msg.data)
@@ -547,6 +590,7 @@ contract CErc20Delegator is
         }
     }
 
+    // 委托执行
     function delegateAndReturn() private returns (bytes memory) {
         (bool success, ) = implementation.delegatecall(msg.data);
 
@@ -565,6 +609,7 @@ contract CErc20Delegator is
     }
 
     /**
+     * 交给具体合约处理
      * @notice Delegates execution to an implementation contract
      * @dev It returns to the external caller whatever the implementation returns or forwards reverts
      */
