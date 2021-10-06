53df41225802621b6a0308c70cfc273b05969eff
Anubis <anubisawooo@163.com>
2021-10-06 15:48:48 +0800

    ComptrollerInterface

--- 015_CErc20Delegator/contracts/ComptrollerInterface.sol
+++ 015_CErc20Delegator/contracts/ComptrollerInterface.sol
@@ -1,25 +1,31 @@
 pragma solidity ^0.5.16;
 
+// 审计接口
 contract ComptrollerInterface {
+    // 标识是一个审计接口
     /// @notice Indicator that this is a Comptroller contract (for inspection)
     bool public constant isComptroller = true;
 
     /*** Assets You Are In ***/
 
+    // 进入市场 what？传入地址数组是什么意思？
     function enterMarkets(address[] calldata cTokens)
         external
         returns (uint256[] memory);
 
+    // 退出市场 what？给个 token 地址就可以了？
     function exitMarket(address cToken) external returns (uint256);
 
     /*** Policy Hooks ***/
 
+    // 铸币允许？
     function mintAllowed(
         address cToken,
         address minter,
         uint256 mintAmount
     ) external returns (uint256);
 
+    // 铸币验证？
     function mintVerify(
         address cToken,
         address minter,
@@ -27,12 +33,14 @@ contract ComptrollerInterface {
         uint256 mintTokens
     ) external;
 
+    // 赎回允许？
     function redeemAllowed(
         address cToken,
         address redeemer,
         uint256 redeemTokens
     ) external returns (uint256);
 
+    // 赎回验证？
     function redeemVerify(
         address cToken,
         address redeemer,
@@ -40,18 +48,21 @@ contract ComptrollerInterface {
         uint256 redeemTokens
     ) external;
 
+    // 借币允许？
     function borrowAllowed(
         address cToken,
         address borrower,
         uint256 borrowAmount
     ) external returns (uint256);
 
+    // 借币验证？
     function borrowVerify(
         address cToken,
         address borrower,
         uint256 borrowAmount
     ) external;
 
+    // 偿还借币允许？
     function repayBorrowAllowed(
         address cToken,
         address payer,
@@ -59,6 +70,7 @@ contract ComptrollerInterface {
         uint256 repayAmount
     ) external returns (uint256);
 
+    // 偿还借币验证？
     function repayBorrowVerify(
         address cToken,
         address payer,
@@ -67,6 +79,7 @@ contract ComptrollerInterface {
         uint256 borrowerIndex
     ) external;
 
+    // 清算借币允许？
     function liquidateBorrowAllowed(
         address cTokenBorrowed,
         address cTokenCollateral,
@@ -75,6 +88,7 @@ contract ComptrollerInterface {
         uint256 repayAmount
     ) external returns (uint256);
 
+    // 清算借币验证
     function liquidateBorrowVerify(
         address cTokenBorrowed,
         address cTokenCollateral,
@@ -84,6 +98,7 @@ contract ComptrollerInterface {
         uint256 seizeTokens
     ) external;
 
+    // 扣押允许？
     function seizeAllowed(
         address cTokenCollateral,
         address cTokenBorrowed,
@@ -92,6 +107,7 @@ contract ComptrollerInterface {
         uint256 seizeTokens
     ) external returns (uint256);
 
+    // 扣押验证？
     function seizeVerify(
         address cTokenCollateral,
         address cTokenBorrowed,
@@ -100,6 +116,7 @@ contract ComptrollerInterface {
         uint256 seizeTokens
     ) external;
 
+    // 转账允许？
     function transferAllowed(
         address cToken,
         address src,
@@ -107,6 +124,7 @@ contract ComptrollerInterface {
         uint256 transferTokens
     ) external returns (uint256);
 
+    // 转账验证？
     function transferVerify(
         address cToken,
         address src,
@@ -116,6 +134,7 @@ contract ComptrollerInterface {
 
     /*** Liquidity/Liquidation Calculations ***/
 
+    // 清算计算扣押代币
     function liquidateCalculateSeizeTokens(
         address cTokenBorrowed,
         address cTokenCollateral,
