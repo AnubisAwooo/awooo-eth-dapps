9a9d57fa5021df494e4f2af0e956159b8c5b0250
Anubis <anubisawooo@163.com>
2021-10-06 17:58:27 +0800

    CTokenInterfaces

--- 015_CErc20Delegator/contracts/CTokenInterfaces.sol
+++ 015_CErc20Delegator/contracts/CTokenInterfaces.sol
@@ -3,106 +3,128 @@ pragma solidity ^0.5.16;
 import "./ComptrollerInterface.sol";
 import "./InterestRateModel.sol";
 
+// token 存储 部分处理
 contract CTokenStorage {
     /**
+     * 监视变量重入检查
      * @dev Guard variable for re-entrancy checks
      */
     bool internal _notEntered;
 
     /**
+     * token 名称
      * @notice EIP-20 token name for this token
      */
     string public name;
 
     /**
+     * token 符号
      * @notice EIP-20 token symbol for this token
      */
     string public symbol;
 
     /**
+     * 代币 精度
      * @notice EIP-20 token decimals for this token
      */
     uint8 public decimals;
 
     /**
+     * 最大借币利率 这是什么数字？ 百分之 0.0005 万分之 0.05 每个块？ 这个数字不是比例，像是 1 单位币每个块的利息 1 是 1e18
      * @notice Maximum borrow rate that can ever be applied (.0005% / block)
      */
-
     uint256 internal constant borrowRateMaxMantissa = 0.0005e16;
 
     /**
+     * 可留作准备金的最大利息比例 谷歌翻译了？？
      * @notice Maximum fraction of interest that can be set aside for reserves
      */
     uint256 internal constant reserveFactorMaxMantissa = 1e18;
 
     /**
+     * 管理员地址
      * @notice Administrator for this contract
      */
     address payable public admin;
 
     /**
+     * 候选管理员地址
      * @notice Pending administrator for this contract
      */
     address payable public pendingAdmin;
 
     /**
+     * 审计合约
      * @notice Contract which oversees inter-cToken operations
      */
     ComptrollerInterface public comptroller;
 
     /**
+     * 利率模型合约
      * @notice Model which tells what the current interest rate should be
      */
     InterestRateModel public interestRateModel;
 
     /**
+     * 初始铸造第一批代币时使用的利率
      * @notice Initial exchange rate used when minting the first CTokens (used when totalSupply = 0)
      */
     uint256 internal initialExchangeRateMantissa;
 
     /**
+     * 目前为准备金预留的利息比例 谷歌翻译
      * @notice Fraction of interest currently set aside for reserves
      */
     uint256 public reserveFactorMantissa;
 
     /**
+     * 上次产生利息的区块编号
      * @notice Block number that interest was last accrued at
      */
     uint256 public accrualBlockNumber;
 
     /**
+     * 自开市以来总赚取利率的累加器
      * @notice Accumulator of the total earned interest rate since the opening of the market
      */
     uint256 public borrowIndex;
 
     /**
+     * 该市场标的的未偿还借款总额 谷歌翻译
      * @notice Total amount of outstanding borrows of the underlying in this market
      */
     uint256 public totalBorrows;
 
     /**
+     * 该市场持有的标的的准备金总额
      * @notice Total amount of reserves of the underlying held in this market
      */
     uint256 public totalReserves;
 
     /**
+     * 流通中的代币总数
      * @notice Total number of tokens in circulation
      */
     uint256 public totalSupply;
 
     /**
+     * 每个账户的代币余额的官方记录s
      * @notice Official record of token balances for each account
      */
     mapping(address => uint256) internal accountTokens;
 
     /**
+     * 额度授权
      * @notice Approved token transfer amounts on behalf of others
      */
     mapping(address => mapping(address => uint256)) internal transferAllowances;
 
     /**
+     * 借用余额信息的容器
      * @notice Container for borrow balance information
+     * 本金 总余额（包括应计利息），在应用最近的余额更改操作后
      * @member principal Total balance (with accrued interest), after applying the most recent balance-changing action
+     * 在全局借贷利率中的序号
      * @member interestIndex Global borrowIndex as of the most recent balance-changing action
      */
     struct BorrowSnapshot {
@@ -111,13 +133,16 @@ contract CTokenStorage {
     }
 
     /**
+     * 账户地址中未偿还借贷余额
      * @notice Mapping of account addresses to outstanding borrow balances
      */
     mapping(address => BorrowSnapshot) internal accountBorrows;
 }
 
+// 代币接口
 contract CTokenInterface is CTokenStorage {
     /**
+     * 标识是代币合约
      * @notice Indicator that this is a CToken contract (for inspection)
      */
     bool public constant isCToken = true;
@@ -125,6 +150,7 @@ contract CTokenInterface is CTokenStorage {
     /*** Market Events ***/
 
     /**
+     * 累积利率事件
      * @notice Event emitted when interest is accrued
      */
     event AccrueInterest(
@@ -135,16 +161,19 @@ contract CTokenInterface is CTokenStorage {
     );
 
     /**
+     * 铸币事件
      * @notice Event emitted when tokens are minted
      */
     event Mint(address minter, uint256 mintAmount, uint256 mintTokens);
 
     /**
+     * 赎回事件
      * @notice Event emitted when tokens are redeemed
      */
     event Redeem(address redeemer, uint256 redeemAmount, uint256 redeemTokens);
 
     /**
+     * 借款事件
      * @notice Event emitted when underlying is borrowed
      */
     event Borrow(
@@ -155,6 +184,7 @@ contract CTokenInterface is CTokenStorage {
     );
 
     /**
+     * 偿还借款事件
      * @notice Event emitted when a borrow is repaid
      */
     event RepayBorrow(
@@ -166,6 +196,7 @@ contract CTokenInterface is CTokenStorage {
     );
 
     /**
+     * 清算借款事件
      * @notice Event emitted when a borrow is liquidated
      */
     event LiquidateBorrow(
@@ -179,16 +210,19 @@ contract CTokenInterface is CTokenStorage {
     /*** Admin Events ***/
 
     /**
+     * 增加候选管理员事件
      * @notice Event emitted when pendingAdmin is changed
      */
     event NewPendingAdmin(address oldPendingAdmin, address newPendingAdmin);
 
     /**
+     * 新管理员事件
      * @notice Event emitted when pendingAdmin is accepted, which means admin is updated
      */
     event NewAdmin(address oldAdmin, address newAdmin);
 
     /**
+     * 新的审核合约事件
      * @notice Event emitted when comptroller is changed
      */
     event NewComptroller(
@@ -197,6 +231,7 @@ contract CTokenInterface is CTokenStorage {
     );
 
     /**
+     * 新的市场利率模型事件
      * @notice Event emitted when interestRateModel is changed
      */
     event NewMarketInterestRateModel(
@@ -205,6 +240,7 @@ contract CTokenInterface is CTokenStorage {
     );
 
     /**
+     * 抵押率改变事件
      * @notice Event emitted when the reserve factor is changed
      */
     event NewReserveFactor(
@@ -213,6 +249,7 @@ contract CTokenInterface is CTokenStorage {
     );
 
     /**
+     * 质押率增加事件
      * @notice Event emitted when the reserves are added
      */
     event ReservesAdded(
@@ -222,6 +259,7 @@ contract CTokenInterface is CTokenStorage {
     );
 
     /**
+     * 质押率降低事件
      * @notice Event emitted when the reserves are reduced
      */
     event ReservesReduced(
@@ -231,11 +269,13 @@ contract CTokenInterface is CTokenStorage {
     );
 
     /**
+     * 转账事件
      * @notice EIP20 Transfer event
      */
     event Transfer(address indexed from, address indexed to, uint256 amount);
 
     /**
+     * 授权事件
      * @notice EIP20 Approval event
      */
     event Approval(
@@ -245,11 +285,13 @@ contract CTokenInterface is CTokenStorage {
     );
 
     /**
+     * 失败事件
      * @notice Failure event
      */
     event Failure(uint256 error, uint256 info, uint256 detail);
 
     /*** User Interface ***/
+    // 用户接口
 
     function transfer(address dst, uint256 amount) external returns (bool);
 
@@ -305,6 +347,7 @@ contract CTokenInterface is CTokenStorage {
     ) external returns (uint256);
 
     /*** Admin Functions ***/
+    // 管理员接口
 
     function _setPendingAdmin(address payable newPendingAdmin)
         external
@@ -327,15 +370,19 @@ contract CTokenInterface is CTokenStorage {
         returns (uint256);
 }
 
+// 代币代理的资产
 contract CErc20Storage {
     /**
+     * 本代币的基础资产
      * @notice Underlying asset for this CToken
      */
     address public underlying;
 }
 
+// 代理资产操作
 contract CErc20Interface is CErc20Storage {
     /*** User Interface ***/
+    // 用户接口
 
     function mint(uint256 mintAmount) external returns (uint256);
 
@@ -358,19 +405,24 @@ contract CErc20Interface is CErc20Storage {
     ) external returns (uint256);
 
     /*** Admin Functions ***/
+    // 管理员接口
 
     function _addReserves(uint256 addAmount) external returns (uint256);
 }
 
+// 委托关系
 contract CDelegationStorage {
     /**
+     * 本合约代理的合约
      * @notice Implementation address for this contract
      */
     address public implementation;
 }
 
+// 委托接口
 contract CDelegatorInterface is CDelegationStorage {
     /**
+     * 代理合约改变事件
      * @notice Emitted when implementation is changed
      */
     event NewImplementation(
@@ -379,6 +431,7 @@ contract CDelegatorInterface is CDelegationStorage {
     );
 
     /**
+     * 更新代理合约
      * @notice Called by the admin to update the implementation of the delegator
      * @param implementation_ The address of the new implementation for delegation
      * @param allowResign Flag to indicate whether to call _resignImplementation on the old implementation
@@ -391,6 +444,7 @@ contract CDelegatorInterface is CDelegationStorage {
     ) public;
 }
 
+// 委托接口
 contract CDelegateInterface is CDelegationStorage {
     /**
      * @notice Called by the delegator on a delegate to initialize it for duty
